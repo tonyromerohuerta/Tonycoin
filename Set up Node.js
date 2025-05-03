05ec6f8fdd3ef545467d05f4b9daab32e4b46d30
Tony Romero Huerta III, LLC <146060376+tonyromerohuerta@users.noreply.github.com>
2025-05-03 06:00:16 -0700

    Create Set up Node.js

---
+++ Set up Node.js
@@ -0,0 +1,16 @@
+- name: Set up Node.js
+         uses: actions/setup-node@v3
+         with:
+           node-version: 16
+
+       # Step 3: Install dependencies
+       - name: Install dependencies
+         run: npm install
+
+       # Step 4: Deploy the contract
+       - name: Deploy Smart Contract
+         env:
+           PRIVATE_KEY: ${{ secrets.PRIVATE_KEY }}
+           INFURA_API_KEY: ${{ secrets.INFURA_API_KEY }}
+         run: |
+           npx hardhat run scripts/deploy.js --network mainnet
