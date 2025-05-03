bbb9206e6836f5f90f563cbbbbeac51d8c2003d7
Tony Romero Huerta III, LLC <146060376+tonyromerohuerta@users.noreply.github.com>
2025-05-03 06:34:19 -0700

    Update Set up Node.js

--- Set up Node.js
+++ Set up Node.js
@@ -1,6 +1,6 @@
 - name: Set up Node.js
          uses: actions/setup-node@v3
-         with: Node.js                         
+         with:                         
               node-version: 16
 
        # Step 3: Install dependencies
@@ -10,10 +10,7 @@
        # Step 4: Deploy the contract
        - name: Deploy Smart Contract
          env:
-           PRIVATE_KEY: ee3cf94d01ed46b6b59a362483f4bd4b${{ secrets.PRIVATE_KEY }}
-           INFURA_API_KEY: curl --url https://mainnet.infura.io/v3/ee3cf94d01ed46b6b59a362483f4bd4b \
-  -X POST \
-  -H "Content-Type: application/json" \
-  -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'${{ secrets.INFURA_API_KEY }}
+           PRIVATE_KEY:${{ secrets.PRIVATE_KEY }}
+           INFURA_API_KEY:${{ secrets.INFURA_API_KEY }}
          run: |
            npx hardhat run scripts/deploy.js --network mainnet
